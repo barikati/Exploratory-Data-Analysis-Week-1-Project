7ff4082f02f56e9a39f05c9f858aafe0008d4a46
barikati <31191225+barikati@users.noreply.github.com>
2017-10-29 08:56:57 -0500

    Add files via upload
    Uploading the files from PC

---
+++ Plot2.R
@@ -0,0 +1,18 @@
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Get the required subset data in between the dates 2007-02-01 and 2007-02-02
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot the requied graph and label the X and Y axis.
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power(Kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
